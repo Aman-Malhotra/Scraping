e7b70858871938201836e5ff451ae1e43931d2ca
Aman-malhotra <official.aman18@gmail.com>
2019-10-22 09:50:59 +0530

    Scraping the quotes vai parse and items

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,11 +1,23 @@
-import scrapy
+import scrapy 
+from ..items  import QuotetutorialItem
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = ['http://howtofashion.in/']
+    start_urls = ['http://quotes.toscrape.com']
 
     def parse(self, response):
-        title = response.css('title::text').extract()
-        yield{'titletext':title}
+        all_div_quotes = response.css('div.quote')
+        
+        items = QuotetutorialItem()
+        
+        for quote in all_div_quotes:
+            title = quote.css('span.text::text').extract()
+            author = quote.css('small.author::text').extract()
+            tag = quote.css('.tag::text').extract()
+            
+            items['title']= title
+            items['author']=author
+            items['tag'] = tag
+            yield items
 
